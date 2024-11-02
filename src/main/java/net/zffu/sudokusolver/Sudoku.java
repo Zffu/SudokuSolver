abb78432a5823a9cba19619dc997e1626c19a880
Zffu <radi0o0@outlook.com>
2024-11-02 20:16:08 +0100

    feat: added bit arrays

--- src/main/java/net/zffu/sudokusolver/Sudoku.java
+++ src/main/java/net/zffu/sudokusolver/Sudoku.java
@@ -5,6 +5,27 @@ package net.zffu.sudokusolver;
  */
 public class Sudoku {
 
+    /**
+     * <p>The bit indices of the presence of numbers in the rows and columns.</p>
+     */
+    public byte[] rows;
+    public byte[] columns;
 
+    /**
+     * <p>Stores if a case is taken or not.</p>
+     */
+    public long[] bitTrace;
+
+    /**
+     * <p>Creates a sudoku with the provided data.</p>
+     * @param rows the bit array of rows.
+     * @param columns the bit array of columns.
+     * @param bitTrace the bit trace array.
+     */
+    public Sudoku(byte[] rows, byte[] columns, long[] bitTrace) {
+        this.rows = rows;
+        this.columns = columns;
+        this.bitTrace = bitTrace;
+    }
 
 }
